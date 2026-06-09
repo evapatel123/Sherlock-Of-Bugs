e6b820113a641fda3a63d98f0278b3bb7b780d5c
Vidhi Maloo <VidhiMaloo@users.noreply.huggingface.co>
2026-06-09 15:35:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,19 @@ from huggingface_hub import InferenceClient
 from sentence_transformers import SentenceTransformer
 import gradio as gr
 import numpy as np 
+
+client=InferenceClient("Qwen/Qwen2.5-7B-Instruct")
+
+def response(message,history):
+    messages=[{"role":"system","content":"You are a friendly chatbot."}]
+
+    if history:
+        messages.extend(history)
+    messages.append({"role":"user","content":message})
+
+    response=client.chat_completion(messages,max_tokens=1000)
+    
+    return response.choices[0].message.content.strip() 
+    
+chatbot=gr.ChatInterface(response)
+chatbot.launch()
